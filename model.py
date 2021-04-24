b5e713a8f922d3018e3972f0d1b75c4d782d9769
che-yjwj <chson0316@gmail.com>
2021-04-24 11:16:41 +0000

    edit vit param.

--- model.py
+++ model.py
@@ -17,10 +17,10 @@ class LitModel(pl.LightningModule):
                 image_size = args.img_size,
                 patch_size = 4,
                 num_classes = 100,
-                dim = 1024,
+                dim = 512,
                 depth = 6,
                 heads = 8,
-                mlp_dim = 2048
+                mlp_dim = 1024
             )
             hidden_layer = 'to_latent'
 
